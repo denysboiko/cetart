997ca755ff7aed111ac42e1f6012213ad9fe24eb
denysboiko <denys.bojko@gmail.com>
2017-03-03 13:48:14 +0200

    new particles

--- js/app.js
+++ js/app.js
@@ -2,6 +2,6 @@
  * Created by denisbojko on 02.03.17.
  */
 /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
-particlesJS.load('particles-js', 'particles.json', function() {
+particlesJS.load('particles-js', 'js/particles.json', function() {
     console.log('callback - particles.js config loaded');
 });
